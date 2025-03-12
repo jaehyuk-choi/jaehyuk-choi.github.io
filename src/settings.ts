ba5a8a224c034916dcfa556655ba9177ffe47158
jaehyuk-choi <s2070553@ed.ac.uk>
2025-03-11 21:36:47 -0400

    image

--- src/settings.ts
+++ src/settings.ts
@@ -32,5 +32,5 @@ export const template = {
 export const seo = {
 	default_title: 'Astro Academia',
 	default_description: 'Astro Academia is a template for academic websites.',
-	default_image: '/images/astro-academia.png',
+	default_image: '/',
 }
